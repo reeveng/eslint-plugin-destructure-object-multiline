519cb7b10d150872e30624442b8a36dfcec5bc73
R Govaert <govaertreeven@gmail.com>
2023-11-17 18:19:42 +0100

    temp

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
 "use strict";
 
 module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
-  ],
-  env: {
-    node: true,
-  },
-  overrides: [
-    {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
-    },
-  ],
+	root: true,
+	extends: [
+		"eslint:recommended",
+		"plugin:eslint-plugin/recommended",
+		"plugin:node/recommended",
+	],
+	env: {
+		node: true,
+	},
+	parser: "babel-eslint",
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			modules: true,
+		},
+	},
+	overrides: [
+		{
+			files: ["tests/**/*.js"],
+			env: { mocha: true },
+		},
+	],
 };
